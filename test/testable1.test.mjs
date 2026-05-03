24d7bcb69e342baa60ae65679641888109ceec0f
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-03 20:50:15 +0300

    Add more specific time tests for testable 1

--- test/testable1.test.mjs
+++ test/testable1.test.mjs
@@ -9,9 +9,18 @@ describe("Testable 1: days until Christmas", () => {
 
   test("25th of December, 0 days until Christmas", () => {
     expect(daysUntilChristmas(new Date("2026-12-25"))).to.equal(0);
+    expect(daysUntilChristmas(new Date("2026-12-25T00:00:00"))).to.equal(0);
+    expect(daysUntilChristmas(new Date("2026-12-25T23:59:59"))).to.equal(0);
   });
 
   test("Boxing day, 364 days until Christmas", () => {
     expect(daysUntilChristmas(new Date("2026-12-26"))).to.equal(364);
+    expect(daysUntilChristmas(new Date("2026-12-26T00:00:00"))).to.equal(364);
+  });
+
+  test("Christmas eve, 1 day until Christmas", () => {
+    expect(daysUntilChristmas(new Date("2026-12-24"))).to.equal(1);
+    expect(daysUntilChristmas(new Date("2026-12-24T00:00:00"))).to.equal(1);
+    expect(daysUntilChristmas(new Date("2026-12-24T23:59:59"))).to.equal(1);
   });
 });
