62f3556817bf7b0c2fa8ea6226bf8652d23ef50a
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-03 20:46:02 +0300

    Add more testable 1 tests

--- test/testable1.test.mjs
+++ test/testable1.test.mjs
@@ -6,4 +6,12 @@ describe("Testable 1: days until Christmas", () => {
   test("1st of December, 24 days until Christmas", () => {
     expect(daysUntilChristmas(new Date("2026-12-01"))).to.equal(24);
   });
+
+  test("25th of December, 0 days until Christmas", () => {
+    expect(daysUntilChristmas(new Date("2026-12-25"))).to.equal(0);
+  });
+
+  test("Boxing day, 364 days until Christmas", () => {
+    expect(daysUntilChristmas(new Date("2026-12-26"))).to.equal(364);
+  });
 });
