6deeb6ffc53ad734773eeff17907ca50b0eb822b
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-04 10:18:02 +0300

    Create copies of untestable 2 file and test as testable 2

---
+++ src/testable2.mjs
@@ -0,0 +1,21 @@
+// This function will stay the same.
+// Only change is to export this so tests can use this directly.
+// Now we can do property-based testing like in tetris shuffle.
+function diceRoll() {
+  const min = 1;
+  const max = 6;
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+// Dice rolls should be passed as parameters like the time in testable1
+export function diceHandValue() {
+  const die1 = diceRoll();
+  const die2 = diceRoll();
+  if (die1 === die2) {
+    // one pair
+    return 100 + die1;
+  } else {
+    // high die
+    return Math.max(die1, die2);
+  }
+}
---
+++ test/testable2.test.mjs
@@ -0,0 +1,10 @@
+import { describe, test } from "vitest";
+import { expect } from "chai";
+import { diceHandValue } from "../src/testable2.mjs";
+
+describe("Testable 2: a dice game", () => {
+  test("todo", () => {
+    // TODO: write proper tests
+    expect(diceHandValue()).to.be.a("number");
+  });
+});
