e8a9fe9579444b83482dca98bd662b08d37bad9b
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-04 10:27:06 +0300

    Add one pair and high die tests for testable 2

--- test/testable2.test.mjs
+++ test/testable2.test.mjs
@@ -1,10 +1,16 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/testable2.mjs";
+import { diceHandValue, diceRoll } from "../src/testable2.mjs";
 
 describe("Testable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+  test("one pair", () => {
+    expect(diceHandValue(2, 2)).to.equal(102);
+    expect(diceHandValue(6, 6)).to.equal(106);
+  });
+
+  test("high die", () => {
+    expect(diceHandValue(2, 1)).to.equal(2);
+    expect(diceHandValue(1, 3)).to.equal(3);
+    expect(diceHandValue(5, 6)).to.equal(6);
   });
 });
