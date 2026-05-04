952a896e1a462b2829821eba5cdd971521791891
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-04 10:19:43 +0300

    Update testable 2 to be testable

--- src/testable2.mjs
+++ src/testable2.mjs
@@ -1,16 +1,14 @@
 // This function will stay the same.
 // Only change is to export this so tests can use this directly.
 // Now we can do property-based testing like in tetris shuffle.
-function diceRoll() {
+export function diceRoll() {
   const min = 1;
   const max = 6;
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
 // Dice rolls should be passed as parameters like the time in testable1
-export function diceHandValue() {
-  const die1 = diceRoll();
-  const die2 = diceRoll();
+export function diceHandValue(die1, die2) {
   if (die1 === die2) {
     // one pair
     return 100 + die1;
