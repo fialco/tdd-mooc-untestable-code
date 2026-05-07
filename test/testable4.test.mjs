146f76e0360366e08c3aa9fa65f6f7dc81527bc8
Fialco <113672867+fialco@users.noreply.github.com>
2026-05-07 13:17:20 +0300

    Add test describe for PasswordService

--- test/testable4.test.mjs
+++ test/testable4.test.mjs
@@ -2,10 +2,18 @@ import { afterEach, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao } from "../src/testable4.mjs";
 
 describe("Testable 4: enterprise application", () => {
-  let service;
-  beforeEach(() => {
-    service = new PasswordService();
-  });
+  describe("PasswordService", () => {
+    let service;
+    beforeEach(() => {
+      service = new PasswordService();
+    });
+
+    test("todo", async () => {
+      // TODO: write proper tests for both PasswordService and PostgresUserDao
+    });
+  })
+
+
 
   async function connectToDb() {
     return db = new pg.Pool({
@@ -15,12 +23,13 @@ describe("Testable 4: enterprise application", () => {
       password: process.env.PGPASSWORD,
       port: process.env.PGPORT,
     });
-
-
   }
+  /*
   afterEach(() => {
     PostgresUserDao.getInstance().close();
   });
+
+  */
 
   test("todo", async () => {
     // TODO: write proper tests for both PasswordService and PostgresUserDao
